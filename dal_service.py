cf1d20b5f2e742b275fe623bcb5df31fea48acd0
Yogev Heskia <yogev911@gmail.com>
2019-09-24 21:54:11 +0300

    added cache for words

--- dal_service.py
+++ dal_service.py
@@ -2,10 +2,13 @@ import json
 
 from pymongo import MongoClient
 import conf
-
-from flask import Flask, escape, request
+from threading import Lock, Thread
+from flask import Flask, request
+from time import sleep
 
 app = Flask(__name__)
+lock = Lock()
+cache = {}
 
 
 class DbClient(object):
@@ -18,13 +21,46 @@ class DbClient(object):
 # db = DbClient()
 
 
-@app.route('/insert',methods=['POST'])
+@app.route('/insert', methods=['POST'])
 def insert():
+    global cache
     data = json.loads(request.data)
-    print(data)
+    lock.acquire()
+    for k, v in data.items():
+        if k in cache:
+            cache[k] += v
+        else:
+            cache[k] = v
+    lock.release()
     # db.words.find()
     return 'ok', 200
 
 
+def words_aggregator(lock):
+    global cache
+    while True:
+        try:
+            lock.acquire()  # will block if lock is already held
+            # TODO
+            print(cache)
+            cache = {}
+            lock.release()
+
+        except Exception as e:
+            print(e.__str__())
+            lock.release()
+        finally:
+            sleep(20)
+
+
 def serve():
-    app.run('0.0.0.0', 8081)
+    try:
+        aggregator = Thread(target=words_aggregator, args=(lock,))
+        aggregator.start()
+        app.run('0.0.0.0', 8081)
+    except Exception as e:
+        print(e.__str__())
+        aggregator.join()
+    finally:
+        if aggregator.is_alive():
+            aggregator.join()
